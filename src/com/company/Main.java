f8e29ab4dd9acd2ac5e72c8c9ce7e11a212170e0
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2021-02-22 21:00:43 +0200

    Optimize programm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,24 @@ package com.company;
 
 public class Main {
 
-    private static final int MAX_COUNT = 100000;
+    private static final int MAX_COUNT = 5000000;
 
     public static void main(String[] args) {
 
         // 100'000 => 9.759
+        // after add break
+        // 100'000 => 0.975
+        // after remove even numbers
+        // 100'000 => 0.487
+
+        // 500'000 => 9.601
+
+        // after add sqrt()
+        // 500'000 => 0.041
+
+
+
+
 
         /*
         System.out.println(isPrime(2)); // true
@@ -14,10 +27,17 @@ public class Main {
         System.out.println(isPrime(5)); // true
         System.out.println(isPrime(19)); // true
 
+        System.out.println(isPrime(6)); // false
         System.out.println(isPrime(15)); // false
         System.out.println(isPrime(21)); // false
         System.out.println(isPrime(49)); // false
          */
+
+
+
+
+
+
 
         long time = System.currentTimeMillis();
 
@@ -32,16 +52,25 @@ public class Main {
 
         System.out.println("Count: " + count);
         System.out.println("Time: " + diff / 1000.0);
+
+
+
+
     }
 
     private static boolean isPrime(int number) {
-        boolean result = true;
-        for (int i = 2; i < number - 1; i++) {
+        if (number == 2) {
+            return true;
+        }
+        if (number % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i * i <= number; i+=2) {
             if (number % i == 0) {
-                result = false;
+                return false;
             }
         }
-        return result;
+        return true;
     }
 
 }
